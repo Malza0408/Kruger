6d44941b12c2b671fee61f81b3f3439c591ed82f
DOHEE <dohee212@naver.com>
2022-03-15 16:35:03 +0900

    [Add] Award Schema 추가

---
+++ back/src/db/schemas/award.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const AwardSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title : {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AwardModel = model("Award", AwardSchema);
+
+export { AwardModel };
