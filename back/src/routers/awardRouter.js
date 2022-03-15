5944dc203e49fa06d0de393526e8f07ddd5f67b3
DOHEE <dohee212@naver.com>
2022-03-15 18:43:36 +0900

    [Add] awardRouter.js 생성, /award/create 작성

---
+++ back/src/routers/awardRouter.js
@@ -0,0 +1,32 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { awardAuthService } from "../services/awardService";
+
+const awardAuthRouter = Router();
+
+awardAuthRouter.post("/award/create", async (req, res, next) => {
+    try {
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+              "headers의 Content-Type을 application/json으로 설정해주세요"
+            );
+        }
+
+        const title = req.body.title
+        const description = req.body.description
+
+        const newAward = await awardAuthService.addAward({
+            title,
+            description,
+        })
+
+        if (newAward.errorMessage) {
+            throw new Error(newAward.errorMessage);
+          }
+      
+          res.status(201).json(newAward);      
+    } catch (error) {
+        next(error)
+    }
+})
