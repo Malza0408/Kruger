9283ef6582be2a7cf61c32ea85baea4f14b325e1
aeebbr <aeebbr@gmail.com>
2022-03-16 16:31:58 +0900

    [Modify] 기존 로컬 테스트용 데이터 주석 처리

--- front/src/components/certificate/Certificate.js
+++ front/src/components/certificate/Certificate.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react';
 import CertificateEditForm from './CertificateEditForm';
 import CertificateCard from './CertificateCard';
 
-const Certificate = () => {
-    const [certificate, setCertificate] = useState({
-        // DB get 전 로컬 테스트 데이터
-        title: '운전면허', 
-        description: '운전 잘 함', 
-        date: '2022-03-15'
-    });
+const Certificate = ({ certificate, setCertificate, portfolioOwnerId, isEditable }) => {
+    // DB get 전 로컬 테스트 데이터. 추후 삭제
+    // const [certificate, setCertificate] = useState({
+    //     id: 'ff9c3279-bbfe-4f9c-8cab-f0f399e6e358',
+    //     title: '운전면허',
+    //     description: '운전 잘 함',
+    //     date: '2022-03-15'
+    // });
+
     const [isEditing, setIsEditing] = useState(false);
 
     return (
@@ -24,7 +26,7 @@ const Certificate = () => {
                 <CertificateCard
                     certificate={certificate}
                     setIsEditing={setIsEditing}
-                    // isEditable={isEditable}
+                    isEditable={isEditable}
                 />
             )}
         </>
