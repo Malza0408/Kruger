7d0ec3fda64514f811d2a513fe11d5b71bf21186
sj_kim <ksj961223@gmail.com>
2022-03-16 12:20:15 +0900

    DB 연결 시도를 위한 코드 수정

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -3,69 +3,64 @@ import { Row, Col, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 const AwardAddForm = ({ setIsAdding, portfolioOwnerId, setAwards }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
-  const handleClick = async (e) => {
-    // 데이터를 추가하기
-    e.preventDefault();
-    try {
-      await axios.post('', {});
-    } catch (err) {
-      console.log('내역 추가에 실패하였습니다.', err);
-    }
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // 데이터를 추가하기
-    await axios.post('url', {
-      title,
-      description,
-    });
-    // 데이터를 다시 불러오고 추가 상태 false로 변환
-    await axios.get('url').then((res) => setAwards(res.data));
-    setIsAdding(false);
-  };
+    const handleSubmit = async (e) => {
+        // 데이터를 추가하기
+        e.preventDefault();
+        const user_id = portfolioOwnerId;
+        await axios.post(`award/create}`, {
+            user_id: portfolioOwnerId,
+            title,
+            description,
+        });
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId='awardAddTitle'>
-        <Form.Control
-          type='text'
-          placeholder='수상내역'
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-      </Form.Group>
-      <Form.Group className='mt-3' controlId='awardAddDescription'>
-        <Form.Control
-          type='text'
-          placeholder='상세내역'
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-        />
-      </Form.Group>
+        await axios
+            .get(`awardlist`, user_id)
+            .then((res) => setAwards(res.data));
+        setIsAdding(false);
+    };
 
-      <Form.Group as={Row} className='text-center mt-3'>
-        <Col sm={{ span: 20 }}>
-          <Button className='me-3' variant='primary' type='submit'>
-            확인
-          </Button>
-          <Button
-            variant='secondary'
-            type='button'
-            onClick={() => {
-              setIsAdding(false);
-            }}
-          >
-            취소
-          </Button>
-        </Col>
-      </Form.Group>
-    </Form>
-  );
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId='awardAddTitle'>
+                <Form.Control
+                    type='text'
+                    placeholder='수상내역'
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}
+                />
+            </Form.Group>
+            <Form.Group className='mt-3' controlId='awardAddDescription'>
+                <Form.Control
+                    type='text'
+                    placeholder='상세내역'
+                    onChange={(e) => {
+                        setDescription(e.target.value);
+                    }}
+                />
+            </Form.Group>
+
+            <Form.Group as={Row} className='text-center mt-3'>
+                <Col sm={{ span: 20 }}>
+                    <Button className='me-3' variant='primary' type='submit'>
+                        확인
+                    </Button>
+                    <Button
+                        variant='secondary'
+                        type='button'
+                        onClick={() => {
+                            setIsAdding(false);
+                        }}
+                    >
+                        취소
+                    </Button>
+                </Col>
+            </Form.Group>
+        </Form>
+    );
 };
 
 export default AwardAddForm;
--- front/src/components/award/Awards.js
+++ front/src/components/award/Awards.js
@@ -6,56 +6,58 @@ import Award from './Award';
 import AwardAddForm from './AwardAddForm';
 
 const Awards = ({ portfolioOwnerId, isEditable }) => {
-  // useState 훅을 통해 awards 상태를 생성함.
-  const [awards, setAwards] = useState([]);
-  // useState 훅을 통해 isAdding 상태를 생성함.
-  const [isAdding, setIsAdding] = useState(false);
-  // 처음 실행 후 ID가 바뀔때마다 렌더링
-  useEffect(async () => {
-    await axios.get(url).then((res) => setAwards(res.data));
-  }, [portfolioOwnerId]);
+    // useState 훅을 통해 awards 상태를 생성함.
+    const [awards, setAwards] = useState([]);
+    // useState 훅을 통해 isAdding 상태를 생성함.
+    const [isAdding, setIsAdding] = useState(false);
+    // 처음 실행 후 ID가 바뀔때마다 렌더링
+    useEffect(async () => {
+        await axios
+            .get('awardlist', portfolioOwnerId)
+            .then((res) => setAwards(res.data));
+    }, [portfolioOwnerId]);
 
-  return (
-    <Card>
-      <Card.Body>
-        <Card.Title>수상이력</Card.Title>
-        {/* 개별 이력을 반복문을 통해 구현 */}
-        {awards.map((award) => {
-          return (
-            <Award
-              key={award.id}
-              award={award}
-              setAwards={setAwards}
-              isEditable={isEditable}
-            />
-          );
-        })}
-        {/* 권한을 가졌을때만 + 버튼 표시 */}
-        {isEditable && (
-          <Row className='text-center mt-3 mb-4'>
-            <Col>
-              <Button
-                variant='primary'
-                onClick={() => {
-                  setIsAdding(true);
-                }}
-              >
-                +
-              </Button>
-            </Col>
-          </Row>
-        )}
-        {/* 추가 가능 상태가 되면 AwardAddForm 컴포넌트를 구현 */}
-        {isAdding && (
-          <AwardAddForm
-            setIsAdding={setIsAdding}
-            setAwards={setAwards}
-            portfolioOwnerId={portfolioOwnerId}
-          />
-        )}
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>수상이력</Card.Title>
+                {/* 개별 이력을 반복문을 통해 구현 */}
+                {awards.map((award) => {
+                    return (
+                        <Award
+                            key={award.id}
+                            award={award}
+                            setAwards={setAwards}
+                            isEditable={isEditable}
+                        />
+                    );
+                })}
+                {/* 권한을 가졌을때만 + 버튼 표시 */}
+                {isEditable && (
+                    <Row className='text-center mt-3 mb-4'>
+                        <Col>
+                            <Button
+                                variant='primary'
+                                onClick={() => {
+                                    setIsAdding(true);
+                                }}
+                            >
+                                +
+                            </Button>
+                        </Col>
+                    </Row>
+                )}
+                {/* 추가 가능 상태가 되면 AwardAddForm 컴포넌트를 구현 */}
+                {isAdding && (
+                    <AwardAddForm
+                        setIsAdding={setIsAdding}
+                        setAwards={setAwards}
+                        portfolioOwnerId={portfolioOwnerId}
+                    />
+                )}
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default Awards;
