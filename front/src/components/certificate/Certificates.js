0f94b65b3d9fa3195b22601a66a82c8116f45ecb
aeebbr <aeebbr@gmail.com>
2022-03-16 16:30:18 +0900

    [Modify] isAdding T/F 로직과 + 버튼을  CertificateCard에서 본 컴포넌트로  이동, map으로 certificate list 하나씩 가져도록 기능 추가, Api.get으로 certificate 업데이트

--- front/src/components/certificate/Certificates.js
+++ front/src/components/certificate/Certificates.js
@@ -1,15 +1,53 @@
-import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, Col, Button } from 'react-bootstrap';
 
 import Certificate from './Certificate';
+import CertificateAddForm from './CertificateAddForm';
 
-const Certificates = () => {
+import * as Api from '../../api';
+
+const Certificates = ({ portfolioOwnerId, isEditable }) => {
+    const [certificate, setCertificate] = useState([]);
+    const [isAdding, setIsAdding] = useState(false);
+
+    useEffect(() => {
+        // "certificatelist/유저id" 엔드포인트로 GET 요청을 하고, certificatelistf를 response의 data로 세팅함.
+        Api.get('certificatelist', portfolioOwnerId).then((res) =>
+            setCertificate(res.data)
+        );
+    }, [portfolioOwnerId]);
+
     return (
         <>
             <Card>
                 <Card.Body>
                     <Card.Title>자격증</Card.Title>
-                    <Certificate />
+                    {isAdding ? (
+                        <CertificateAddForm
+                            setIsAdding={setIsAdding}
+                            setCertificate={setCertificate}
+                            portfolioOwnerId={portfolioOwnerId}
+                        />
+                    ) : (
+                        certificate.map((certificate) => {
+                            return (
+                                <Certificate
+                                    certificate={certificate}
+                                    setCertificate={setCertificate}
+                                    portfolioOwnerId={portfolioOwnerId}
+                                    isEditable={isEditable}
+                                />
+                            );
+                        })
+                    )}
+                    <Col>
+                        <Button
+                            variant="primary"
+                            onClick={() => setIsAdding(true)}
+                        >
+                            +
+                        </Button>{' '}
+                    </Col>
                 </Card.Body>
             </Card>
         </>
