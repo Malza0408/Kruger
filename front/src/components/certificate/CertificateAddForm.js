c1d24e0f5a30f169c04c9cd69a81656d5ab66c51
aeebbr <aeebbr@gmail.com>
2022-03-16 16:36:21 +0900

    [Modify] 백 연결(추가 등록 데이터 DB에 post)

--- front/src/components/certificate/CertificateAddForm.js
+++ front/src/components/certificate/CertificateAddForm.js
@@ -2,15 +2,38 @@ import React, { useState } from 'react';
 import { Button, Form, Card, Col, Row } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 
-const CertificateAddForm = ({ setIsAdding }) => {
+import * as Api from '../../api';
+
+const CertificateAddForm = ({ setIsAdding, setCertificate, portfolioOwnerId }) => {
+    const user_id = portfolioOwnerId;
     const [title, setTitle] = useState('');
-    const [description, setDescription] = useState();
+    const [description, setDescription] = useState('');
     const [date, setDate] = useState(new Date());
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        try {
+            // "user/register" 엔드포인트로 post요청함.
+            const res = await Api.post('certificate/create', {
+                user_id,
+                title,
+                description,
+                date
+            });
+
+            // 자격증 정보는 response의 data임.
+            const updatedCertificate = res.data;
+
+            // 해당 자격증 정보로 certificate을 세팅함.
+            setCertificate(updatedCertificate);
+        } catch (err) {
+            console.log('등록 실패', err); // 테스트용. 추후 삭제
+        }
+
         setIsAdding(false);
     };
+
     return (
         <>
             <Card>
