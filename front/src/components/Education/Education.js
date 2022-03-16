484c209dc489705cc63640896080097d7be653ea
Malza0408 <parkjh04080@gmail.com>
2022-03-16 15:18:45 +0900

    [Add]: comment, prettier

--- front/src/components/Education/Education.js
+++ front/src/components/Education/Education.js
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
-import { Card, Button } from "react-bootstrap";
-import EducationBody from "./EducationBody";
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import EducationBody from './EducationBody';
 
-const Education = (props) => {
-  const [educationBody, setEducationBody] = useState([]);
+const Education = ({ portfolioOwnerId, isEditable }) => {
+    const [educationBody, setEducationBody] = useState([]);
 
-  const addEducation = () => {
-    setEducationBody([
-      ...educationBody,
-      <EducationBody key={educationBody.length} />,
-    ]);
-  };
-  return (
-    <Card>
-      <Card.Body>
-        <EducationBody />
-        {educationBody?.map((body, index) => {
-          return body;
-        })}
-        <div className="mt-3 text-center mb-1">
-          <div className="col-sm-12">
-            <Button variant="primary" onClick={addEducation}>
-              +
-            </Button>
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
-  );
+    // + 버튼으로 추가하면 EducationBody 추가해준다.
+    const addEducation = () => {
+        setEducationBody([
+            ...educationBody,
+            <EducationBody key={educationBody.length} />,
+        ]);
+    };
+    return (
+        <Card>
+            <Card.Body>
+                <EducationBody portfolioOwnerId={portfolioOwnerId} />
+                {/* + 버튼으로 추가하면 educationBody 배열로 들어간다. 배열을 돌면서 렌더링 */}
+                {educationBody?.map((body, index) => {
+                    return body;
+                })}
+                {/* Education을 추가하는 버튼 */}
+                <div className='mt-3 text-center mb-1'>
+                    <div className='col-sm-12'>
+                        <Button variant='primary' onClick={addEducation}>
+                            +
+                        </Button>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default Education;
--- front/src/components/Education/EducationBody.js
+++ front/src/components/Education/EducationBody.js
@@ -1,89 +1,105 @@
-import React, { useState } from "react";
-import { Card, Row, Col, Button } from "react-bootstrap";
-import EducationForm from "./EducationForm";
-const EducationBody = ({}) => {
-  //   const [school, setSchool] = useState("");
-  const [isEditing, setIsEditing] = useState(false);
-  const [schoolInput, setSchoolInput] = useState("");
-  const [majorInput, setMajorInput] = useState("");
-  const [radioValue, setRadioValue] = useState("");
-  const [schoolName, setSchoolName] = useState("");
-  const [majorName, setMajorName] = useState("");
-  const [schoolState, setSchoolState] = useState("");
+import React, { useState } from 'react';
+import { Card, Row, Col, Button } from 'react-bootstrap';
+import EducationForm from './EducationForm';
+const EducationBody = ({ portfolioOwnerId }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [schoolInput, setSchoolInput] = useState('');
+    const [majorInput, setMajorInput] = useState('');
+    const [radioValue, setRadioValue] = useState('');
+    // 학교 이름 값이 들어간다.
+    const [schoolName, setSchoolName] = useState('');
+    // 전공 이름 값이 들어간다.
+    const [majorName, setMajorName] = useState('');
+    // 재학중인지, 졸업인지의 값이 들어간다.
+    const [schoolState, setSchoolState] = useState('');
 
-  const handleEditing = (e) => {
-    setIsEditing(!isEditing);
-  };
-  const handleSchoolOnChange = (e) => {
-    setSchoolInput(e.target.value);
-  };
-  const handleMajorOnChange = (e) => {
-    setMajorInput(e.target.value);
-  };
-
-  const radioCheckTest = (e) => {
-    setRadioValue(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (schoolInput && majorInput && radioValue) {
-      setIsEditing(false);
-      setSchoolName(schoolInput);
-      setMajorName(majorInput);
-      setSchoolState(radioValue);
-      setSchoolInput("");
-      setMajorInput("");
-    }
-  };
-  return (
-    <>
-      {isEditing ? (
-        <EducationForm
-          handleSchoolOnChange={handleSchoolOnChange}
-          handleMajorOnChange={handleMajorOnChange}
-          schoolInput={schoolInput}
-          majorInput={majorInput}
-          radioCheckTest={radioCheckTest}
-          handleSubmit={handleSubmit}
-          handleEditing={handleEditing}
-        />
-      ) : (
+    const handleEditing = (e) => {
+        setIsEditing(!isEditing);
+    };
+    const handleSchoolOnChange = (e) => {
+        setSchoolInput(e.target.value);
+    };
+    const handleMajorOnChange = (e) => {
+        setMajorInput(e.target.value);
+    };
+    const handleCheckOnClick = (e) => {
+        setRadioValue(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // 칸을 비워놨다면 값을 받지 않는다.
+        if (schoolInput && majorInput && radioValue) {
+            setIsEditing(false);
+            setSchoolName(schoolInput);
+            setMajorName(majorInput);
+            setSchoolState(radioValue);
+            setSchoolInput('');
+            setMajorInput('');
+        }
+    };
+    return (
         <>
-          <Card.Title>학력</Card.Title>
-          <Card.Text className="mb-0">
-            <Row className="align-items-center">
-              <Col>
-                {schoolName ? <span>{schoolName}</span> : <span>OO학교</span>}
-                <br />
-                {majorName ? (
-                  <>
-                    <span className="text-muted">{majorName} </span>
-                    <span className="text-muted">({schoolState})</span>
-                  </>
-                ) : (
-                  <>
-                    <span className="text-muted">OO전공 </span>
-                    <span className="text-muted">(재학중)</span>
-                  </>
-                )}
-              </Col>
-              <Col className="col-lg-1">
-                <Button
-                  className=""
-                  variant="outline-info"
-                  size="sm"
-                  onClick={handleEditing}
-                >
-                  편집
-                </Button>
-              </Col>
-            </Row>
-          </Card.Text>
-          <hr></hr>
+            {/* 편집중이라면 Form을, 그렇지 않다면 메인 학력창을 보여준다. */}
+            {isEditing ? (
+                <EducationForm
+                    handleSchoolOnChange={handleSchoolOnChange}
+                    handleMajorOnChange={handleMajorOnChange}
+                    schoolInput={schoolInput}
+                    majorInput={majorInput}
+                    handleCheckOnClick={handleCheckOnClick}
+                    handleSubmit={handleSubmit}
+                    handleEditing={handleEditing}
+                    portfolioOwnerId={portfolioOwnerId}
+                />
+            ) : (
+                <>
+                    <Card.Title>학력</Card.Title>
+                    <Card.Text className='mb-0'>
+                        <Row className='align-items-center'>
+                            <Col>
+                                {schoolName ? (
+                                    <span>{schoolName}</span>
+                                ) : (
+                                    <span>OO학교</span>
+                                )}
+                                <br />
+                                {majorName ? (
+                                    <>
+                                        <span className='text-muted'>
+                                            {majorName}{' '}
+                                        </span>
+                                        <span className='text-muted'>
+                                            ({schoolState})
+                                        </span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className='text-muted'>
+                                            OO전공{' '}
+                                        </span>
+                                        <span className='text-muted'>
+                                            (재학중)
+                                        </span>
+                                    </>
+                                )}
+                            </Col>
+                            <Col className='col-lg-1'>
+                                <Button
+                                    className=''
+                                    variant='outline-info'
+                                    size='sm'
+                                    onClick={handleEditing}
+                                >
+                                    편집
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Card.Text>
+                    <hr></hr>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default EducationBody;
