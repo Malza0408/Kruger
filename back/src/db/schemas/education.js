97e164ce721cc8825b5c8e7be4edb0631b08e951
guswn2521@gmail.com <guswn2521@gmail.com>
2022-03-16 15:52:09 +0900

    [Modify] api의 req와 body가 달라서 필드 수정

--- back/src/db/schemas/education.js
+++ back/src/db/schemas/education.js
@@ -18,7 +18,7 @@ const EducationSchema = new Schema(
             type: String,
             required: true
         },
-        graduation: {
+        position: {
             type: String,
             enum: ['재학중', '학사졸업', '석사졸업', '박사졸업'],
             required: false
