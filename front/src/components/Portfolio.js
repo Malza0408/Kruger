74e2b2b09cc05f08572b74c0ed7e477d45a2571a
Malza0408 <parkjh04080@gmail.com>
2022-03-17 16:17:50 +0900

    [Modify]: Portfolio.js

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,10 +5,10 @@ import { Container, Col, Row } from 'react-bootstrap';
 import { UserStateContext } from '../App';
 import * as Api from '../api';
 import User from './user/User';
-// import Educations from './education/Educations';
+import Educations from './education/Educations';
 import Awards from './award/Awards';
 import Projects from './project/Projects';
-// import Certificates from './certificate/Certificates';
+import Certificates from './certificate/Certificates';
 
 function Portfolio() {
     const navigate = useNavigate();
@@ -65,14 +65,14 @@ function Portfolio() {
                     />
                 </Col>
                 <Col>
-                    {/* <div className='mb-2'>
+                    <div className='mb-2'>
                         <Educations
                             portfolioOwnerId={portfolioOwner.id}
                             isEditable={
                                 portfolioOwner.id === userState.user?.id
                             }
                         />
-                    </div> */}
+                    </div>
                     <div className='mb-2'>
                         <Awards
                             portfolioOwnerId={portfolioOwner.id}
@@ -89,14 +89,14 @@ function Portfolio() {
                             }
                         />
                     </div>
-                    {/* <div className='mb-2'>
+                    <div className='mb-2'>
                         <Certificates
                             portfolioOwnerId={portfolioOwner.id}
                             isEditable={
                                 portfolioOwner.id === userState.user?.id
                             }
                         />
-                    </div> */}
+                    </div>
                 </Col>
             </Row>
         </Container>
