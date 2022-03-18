1b846a3503f2bc4e5b2bf98b6c3960d89dbf6043
sj_kim <ksj961223@gmail.com>
2022-03-18 10:04:28 +0900

    gitignore 수정

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,10 +1,5 @@
-import mongoose from "mongoose";
-import { User } from "./models/User";
-import { Education } from './models/Education';
-import { Award } from './models/Award';
-import { Certificate } from './models/Certificate';
-import { Project } from './models/Project';
-
+import mongoose from 'mongoose';
+import { User } from './models/User';
 
 const DB_URL =
     process.env.MONGODB_URL ||
@@ -20,4 +15,4 @@ db.on('error', (error) =>
     console.error('MongoDB 연결에 실패하였습니다...\n' + DB_URL + '\n' + error)
 );
 
-export { User, Award, Certificate,Education, Project };
+export { User };
