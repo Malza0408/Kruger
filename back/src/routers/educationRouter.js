095d94f2659661c0db1955fdf92297ecbcf0cf99
guswn2521@gmail.com <guswn2521@gmail.com>
2022-03-18 12:17:46 +0900

    [Modify] login_required 추가

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -5,40 +5,50 @@ import { userAuthService } from '../services/userService';
 import { EducationService } from '../services/EducationService';
 
 const educationRouter = Router();
-educationRouter.post('/education/create', async (req, res, next) => {
-    try {
-        if (is.emptyObject(req.body)) {
-            throw new Error(
-                'headers의 Content-Type을 application/json으로 설정해주세요'
-            );
-        }
-        const { user_id, school, major, position } = req.body;
+educationRouter.post(
+    '/education/create',
+    login_required,
+    async (req, res, next) => {
+        try {
+            if (is.emptyObject(req.body)) {
+                throw new Error(
+                    'headers의 Content-Type을 application/json으로 설정해주세요'
+                );
+            }
+            const { user_id, school, major, position } = req.body;
 
-        const newEducation = await EducationService.createEducation({
-            user_id,
-            school,
-            major,
-            position
-        });
-        if (newEducation.errorMessage) {
-            throw new Error(newEducation.errorMessage);
+            const newEducation = await EducationService.createEducation({
+                user_id,
+                school,
+                major,
+                position
+            });
+            if (newEducation.errorMessage) {
+                throw new Error(newEducation.errorMessage);
+            }
+            console.log('education 생성되었습니다.');
+            res.status(201).json(newEducation);
+        } catch (error) {
+            next(error);
         }
-        console.log('education 생성되었습니다.');
-        res.status(201).json(newEducation);
-    } catch (error) {
-        next(error);
     }
-});
+);
 // user의 전체 education 목록 가져오기
-educationRouter.get('/educationlist/:user_id', async (req, res, next) => {
-    try {
-        const user_id = req.params.user_id;
-        const educationList = await EducationService.getEducations({ user_id });
-        res.status(200).send(educationList);
-    } catch (error) {
-        next(error);
+educationRouter.get(
+    '/educationlist/:user_id',
+    login_required,
+    async (req, res, next) => {
+        try {
+            const user_id = req.params.user_id;
+            const educationList = await EducationService.getEducations({
+                user_id
+            });
+            res.status(200).send(educationList);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 educationRouter.get('/educations/:id', async (req, res, next) => {
     try {
@@ -53,23 +63,27 @@ educationRouter.get('/educations/:id', async (req, res, next) => {
     }
 });
 
-educationRouter.put('/educations/:id', async (req, res, next) => {
-    try {
-        const education_id = req.params.id;
-        const { school, major, position } = req.body ?? null;
-        const toUpdate = { school, major, position };
-        const updatedEducation = await EducationService.setEducation({
-            education_id,
-            toUpdate
-        });
-        if (updatedEducation.errorMessage) {
-            throw new Error(errorMessage);
+educationRouter.put(
+    '/educations/:id',
+    login_required,
+    async (req, res, next) => {
+        try {
+            const education_id = req.params.id;
+            const { school, major, position } = req.body ?? null;
+            const toUpdate = { school, major, position };
+            const updatedEducation = await EducationService.setEducation({
+                education_id,
+                toUpdate
+            });
+            if (updatedEducation.errorMessage) {
+                throw new Error(errorMessage);
+            }
+            console.log('수정되었습니다.');
+            res.status(200).json(updatedEducation);
+        } catch (error) {
+            next(error);
         }
-        console.log('수정되었습니다.');
-        res.status(200).json(updatedEducation);
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 export { educationRouter };
