1f15c4ad3d0593883854fcb7c4508aa99a46e424
DOHEE <dohee212@naver.com>
2022-03-19 18:51:05 +0900

    [Modify] picture에 디폴트 값으로 랜덤 고양이 사진 사이트 링크

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -25,7 +25,8 @@ const UserSchema = new Schema(
         },
         picture: {
             type: String,
-            required: false
+            required: false,
+            default: 'http://placekitten.com/200/200'
         }
     },
     {
