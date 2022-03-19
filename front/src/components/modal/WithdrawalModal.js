5bc1d3a8a8b9943cdf176bca7ddf85660450f48b
sj_kim <ksj961223@gmail.com>
2022-03-19 17:02:06 +0900

    [Add] 회원탈퇴 확인 모달 추가

---
+++ front/src/components/modal/WithdrawalModal.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+const WithdrawalModal = ({ show, handleClose }) => {
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>회원 탈퇴</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>회원을 탈퇴하시겠습니까?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary">로그아웃</Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                        취소
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default WithdrawalModal;
