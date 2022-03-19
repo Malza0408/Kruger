26830b86a2de347bb58a79921e0aa259606a218a
guswn2521@gmail.com <guswn2521@gmail.com>
2022-03-19 16:26:00 +0900

    [Add] github login ERR_REQUIRE_ESM 에러

--- back/src/routers/authRouter.js
+++ back/src/routers/authRouter.js
@@ -4,6 +4,7 @@ import { login_required } from '../middlewares/login_required';
 import jwt from 'jsonwebtoken';
 
 const authRouter = Router();
+//구글
 authRouter.get(
     '/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
@@ -22,5 +23,43 @@ authRouter.get(
     //     res.redirect('/');
     // }
 );
+// 깃허브
+authRouter.get('/auth/github', async (req, res, next) => {
+    const clientId = process.env.GITHUB_CLIENT_ID;
+    const redirectUri = 'http://localhost:3001/auth/github/callback';
+    const uri = 'https://github.com/login/oauth/authorize';
+    res.redirect(
+        `${uri}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user user:email`
+    );
+});
 
+authRouter.get('/auth/github/callback', async (req, res, next) => {
+    try {
+        const uri = 'https://github.com/login/oauth/access_token';
+        const config = {
+            code: req.query.code,
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        };
+        console.log(config);
+
+        const params = new URLSearchParams(config).toString();
+        console.log(params);
+        const finalUrl = `${uri}?${params}`;
+        console.log(finalUrl);
+        const tokenRequest = await import('node-fetch')
+            .then(({ default: fetch }) => {
+                fetch(finalUrl, {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json'
+                    }
+                });
+            })
+            .json();
+        console.log(tokenRequest);
+    } catch (error) {
+        next(error);
+    }
+});
 export { authRouter };
