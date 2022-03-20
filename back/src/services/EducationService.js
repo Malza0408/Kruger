4e1ecf37853a6e341628f880bfb4776e317361f6
guswn2521@gmail.com <guswn2521@gmail.com>
2022-03-20 13:53:04 +0900

    [Modify] 변수,함수명 통일

--- back/src/services/EducationService.js
+++ back/src/services/EducationService.js
@@ -4,34 +4,38 @@ import { v4 as uuidv4 } from 'uuid';
 import { EducationModel } from '../db/schemas/education';
 
 class EducationService {
-    static async createEducation({ user_id, school, major, position }) {
+    static async addEducation({ user_id, school, major, position }) {
+        // id 는 유니크 값 부여
         const id = uuidv4();
         const newEducation = { id, user_id, school, major, position };
 
+        // db에 저장
         const createdNewEducation = await Education.create({ newEducation });
-        createdNewEducation.errorMessage = null;
 
         return createdNewEducation;
     }
-    static async getEducation({ education_id }) {
-        const education = await Education.findByEducationId({ education_id });
 
+    static async getEducations({ user_id }) {
+        const educations = await Education.findAll({ user_id });
+        return educations;
+    }
+
+    static async getEducationInfo({ education_id }) {
+        const education = await Education.findById({ education_id });
+
+        // db에서 찾지 못한 경우, 에러 메시지 반환
         if (!education) {
-            const errorMessage =
-                '해당하는 학력이 없습니다. 다시 한 번 확인해 주세요.';
+            const errorMessage = '삭제되었거나 등록되지 않은 학력입니다.';
             throw new Error(errorMessage);
         }
         return education;
     }
-    static async getEducations({ user_id }) {
-        const educationList = await Education.findAll({ user_id });
-        return educationList;
-    }
+
     static async setEducation({ education_id, toUpdate }) {
-        let education = await Education.findByEducationId({ education_id });
+        let education = await Education.findById({ education_id });
+
         if (!education) {
-            const errorMessage =
-                '학력을 등록한 내역이 없습니다. 다시 한 번 확인해주세요.';
+            const errorMessage = '삭제되었거나 등록되지 않은 학력입니다.';
             throw new Error(errorMessage);
         }
         if (toUpdate.school) {
@@ -64,14 +68,14 @@ class EducationService {
         return education;
     }
     static async deleteEducation({ education_id }) {
-        const targetEducatoin = await Education.findByEducationId({
+        const education = await Education.findById({
             education_id
         });
-        if (!targetEducatoin) {
+        if (!education) {
             const errorMessage = '해당하는 학력이 없습니다. 다시 확인해주세요.';
             throw new Error(errorMessage);
         }
-        await Education.delete({ education_id });
+        await Education.deleteById({ education_id });
         return;
     }
 }
