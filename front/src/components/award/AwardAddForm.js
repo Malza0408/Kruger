29d7db0d7a8b2588ff5c716996b329fbbb8d2b4f
aeebbr <aeebbr@gmail.com>
2022-03-21 01:06:38 +0900

    [Modify] 공란 제출 경고문구 로직 수정

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import { Row, Col, Button, Form } from 'react-bootstrap';
 import * as Api from '../../api';
-import InputEmpty from '../InputEmpty'
 
 const AwardAddForm = ({ setIsAdding, portfolioOwnerId, setAwards }) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
-    const [isInputEmpty, setIsInputEmpty] = useState(false);
+    const [isTitleEmpty, setIsTitleEmpty] = useState(false);
+    const [isDescriptionEmpty, setIsDescriptionEmpty] = useState(false);
 
     const handleSubmit = async (e) => {
         // 데이터를 추가하기
         e.preventDefault();
         const user_id = portfolioOwnerId;
 
-        // 빈 인풋 제출 검사
-        setIsInputEmpty(
-            InputEmpty({ title, description})
-        );
+        // title 공란이면 true
+        setIsTitleEmpty(!title);
+        // description 공란이면 true
+        setIsDescriptionEmpty(!description);
 
         await Api.post(`award/create`, {
             user_id: portfolioOwnerId,
             title,
-            description,
+            description
         });
 
         await Api.get(`awardlist`, user_id).then((res) => setAwards(res.data));
@@ -31,47 +31,47 @@ const AwardAddForm = ({ setIsAdding, portfolioOwnerId, setAwards }) => {
 
     return (
         <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='awardAddTitle'>
+            <Form.Group controlId="awardAddTitle">
                 <Form.Control
-                    type='text'
-                    placeholder='수상내역'
+                    type="text"
+                    placeholder="수상내역"
                     onChange={(e) => {
                         setTitle(e.target.value);
                     }}
                 />
-                {isInputEmpty.isTitleEmpty && (
+                {isTitleEmpty && (
                     <Form.Text className="text-success">
                         수상내역을 입력해주세요
                     </Form.Text>
                 )}
             </Form.Group>
-            <Form.Group className='mt-3' controlId='awardAddDescription'>
+            <Form.Group className="mt-3" controlId="awardAddDescription">
                 <Form.Control
-                    type='text'
-                    placeholder='상세내역'
+                    type="text"
+                    placeholder="상세내역"
                     onChange={(e) => {
                         setDescription(e.target.value);
                     }}
                 />
-                {isInputEmpty.isDescriptionEmpty && (
+                {isDescriptionEmpty && (
                     <Form.Text className="text-success">
                         상세내역을 입력해주세요
                     </Form.Text>
                 )}
             </Form.Group>
-            <Form.Group as={Row} className='text-center mt-3'>
-                    {isInputEmpty && (
-                        <Form.Text className="text-success">
-                            빠짐 없이 입력해주세요
-                        </Form.Text>
-                    )}
+            <Form.Group as={Row} className="text-center mt-3">
+                {(isTitleEmpty || isDescriptionEmpty) && (
+                    <Form.Text className="text-success">
+                        빠짐 없이 입력해주세요
+                    </Form.Text>
+                )}
                 <Col sm={{ span: 20 }}>
-                    <Button className='me-3' variant='primary' type='submit'>
+                    <Button className="me-3" variant="primary" type="submit">
                         확인
                     </Button>
                     <Button
-                        variant='secondary'
-                        type='button'
+                        variant="secondary"
+                        type="button"
                         onClick={() => {
                             setIsAdding(false);
                         }}
