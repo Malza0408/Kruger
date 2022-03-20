4605cdcdb878781f6dca9a3fefb3d03c7bc51d09
DOHEE <dohee212@naver.com>
2022-03-21 02:03:00 +0900

    [Modify] project router 파일이름과 라우터 이름 통일

--- back/src/app.js
+++ back/src/app.js
@@ -5,7 +5,7 @@ import { userAuthRouter } from './routers/userRouter';
 import { educationRouter } from './routers/educationRouter';
 import { awardRouter } from './routers/awardRouter';
 import { certificateRouter } from './routers/certificateRouter';
-import { projectAuthRouter } from './routers/projectRouter';
+import { projectRouter } from './routers/projectRouter';
 import { noteRouter } from './routers/noteRouter';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import { authRouter } from './routers/authRouter';
@@ -33,7 +33,7 @@ app.use(authRouter);
 app.use(educationRouter);
 app.use(awardRouter);
 app.use(certificateRouter);
-app.use(projectAuthRouter);
+app.use(projectRouter);
 app.use(noteRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
