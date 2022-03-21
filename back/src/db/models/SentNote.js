f809dfa3422f2566de8ed4b923e6556ec9fa8cc7
DOHEE <dohee212@naver.com>
2022-03-21 15:22:23 +0900

    [Modify] db를 보낸 쪽지와 받은 쪽지로 분할

---
+++ back/src/db/models/SentNote.js
@@ -0,0 +1,38 @@
+import { SentNoteModel } from '../schemas/sentNote';
+
+class SentNote {
+    static async create({ newNote }) {
+        const createdNewNote = await SentNoteModel.create(newNote);
+        return createdNewNote;
+    }
+
+    static async findAll() {
+        const notes = await SentNoteModel.find({})
+            .populate('toUser')
+            .populate('fromUser');
+        return notes;
+    }
+
+    static async findById({ noteId }) {
+        const note = await SentNoteModel.findOne({ id: noteId })
+            .populate('toUser')
+            .populate('fromUser');
+        return note;
+    }
+
+    static async updateCheck({ noteId }) {
+        await SentNoteModel.findOneAndUpdate(
+            { id: noteId },
+            { check: true },
+            { returnOriginal: false }
+        );
+        return;
+    }
+
+    static async deleteById({ noteId }) {
+        await SentNoteModel.deleteOne({ id: noteId });
+        return;
+    }
+}
+
+export { SentNote };
