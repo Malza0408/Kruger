c0803ab4bac318a8bc12cfd4a631dc3750cd64d1
aeebbr <aeebbr@gmail.com>
2022-03-23 03:12:38 +0900

    [Modify] 수신함 텍스트 디자인 수정, 삭제 버튼 위치 조정, 읽음 기능 구현

--- front/src/components/note/NoteListTake.js
+++ front/src/components/note/NoteListTake.js
@@ -1,17 +1,28 @@
-import { Container, Form, Row, Col, Button, Card } from 'react-bootstrap';
+import {
+    Container,
+    Form,
+    Row,
+    Col,
+    Button,
+    Card,
+    Badge
+} from 'react-bootstrap';
 import * as Api from '../../api';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-
 
 const NoteListTake = ({ key, takeNote, setTakeNote }) => {
     const navigate = useNavigate();
+
+    const handleRead = async () => {
+        await Api.put(`takenNotes/${takeNote.id}`);
+    };
 
     const handleDelete = async (e) => {
         e.preventDefault();
 
         await Api.delete(`takenNotes/${takeNote.id}`);
 
-        // await Api.get('sentNotelist', user_id).then((res) => setSendNote(res.data));
         await Api.get(`takenNotelist`).then((res) => {
             setTakeNote(res.data);
         });
@@ -23,8 +34,48 @@ const NoteListTake = ({ key, takeNote, setTakeNote }) => {
                 <Row>
                     <Col>
                         <Card.Title>
-                            {takeNote.fromUser.name}가 보낸 쪽지
+                            <span>
+                                <strong>{takeNote.fromUser.name}</strong>
+                            </span>
+                            <span className="text-muted">
+                                <small>이(가) 보낸 쪽지</small>
+                            </span>
                         </Card.Title>
+                    </Col>
+                    <Col>
+                        <span className="text-muted">
+                            {takeNote.check ? (
+                                <Badge pill bg="secondary">
+                                    읽음
+                                </Badge>
+                            ) : (
+                                <Badge pill bg="success">
+                                    읽지 않음
+                                </Badge>
+                            )}
+                            <small>{takeNote.check}</small>
+                        </span>
+                    </Col>
+                </Row>
+                <Col>
+                    <Card.Link
+                        onClick={() => {
+                            navigate(`/note/${takeNote.id}`);
+                            handleRead();
+                        }}
+                    >
+                        <span>
+                            <strong>{takeNote.title}</strong>
+                        </span>
+                    </Card.Link>
+                </Col>
+                <Row>
+                    <Col>
+                        <Card.Text>
+                            <span className="text-muted">
+                                {takeNote.createdAt}
+                            </span>
+                        </Card.Text>
                     </Col>
                     <Col>
                         <Button
@@ -37,23 +88,9 @@ const NoteListTake = ({ key, takeNote, setTakeNote }) => {
                         </Button>{' '}
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
-                        <Card.Link onClick={() => navigate('/note/write')}>
-                            {takeNote.title}
-                        </Card.Link>
-                    </Col>
-                    <Col>
-                        <Card.Text>
-                            <span className="text-muted">
-                                {takeNote.createdAt}
-                            </span>
-                        </Card.Text>
-                    </Col>
-                </Row>
-                <Card.Text>
+                {/* <Card.Text>
                     <span className="text-muted">{takeNote.content}</span>
-                </Card.Text>
+                </Card.Text> */}
             </Card.Body>
         </Card.Text>
     );
