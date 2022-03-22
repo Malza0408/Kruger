88728a9367bc9b14461acd88cdcab473b07389a5
aeebbr <aeebbr@gmail.com>
2022-03-23 03:05:44 +0900

    [Modify] note 상세 페이지 구현

--- front/src/components/note/NoteDescription.js
+++ front/src/components/note/NoteDescription.js
@@ -5,29 +5,61 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const NoteDescription = () => {
     const params = useParams();
-    // const [noteId, setNoteId] = useState('');
+    const [user, setUser] = useState(null);
     const [note, setNote] = useState('');
 
     useEffect(() => {
-        // setNoteId(params.noteId);
+        Api.get(`user/current`).then((res) => setUser(res.data));
+    }, []);
 
+    useEffect(() => {
         Api.get(`sentNotes/${params.noteId}`).then((res) => {
             setNote(res.data);
         });
-        // Api.get(`takenNotelist`).then((res) => {
-        //     setTakeNote(res.data);
-        // });
     }, [params]);
-    console.log({note});
-    
+
     return (
-        <div>
-            <div>{note.title}</div>
-            <div>{note.content}</div>
-            <div>{note.createdAt}</div>
-            {/* {/* <div>{note.fromUser.name}</div> */}
-            {/* <div>{note.toUser.name}</div>  */}
-        </div>
+        <>
+            <Card.Text as={Col}>
+                <Card.Body>
+                    <Row>
+                        <Col>
+                            <Card.Title>
+                                <span>
+                                    <strong>{note.fromUser?.name}</strong>
+                                </span>
+
+                                <span className="text-muted">
+                                    <small>가 </small>
+                                </span>
+                                <span>
+                                    <strong>{note.toUser?.name}</strong>
+                                </span>
+
+                                <span className="text-muted">
+                                    <small>에게 보낸 쪽지</small>
+                                </span>
+                            </Card.Title>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <span>
+                                <strong>{note.title}</strong>
+                            </span>
+                        </Col>
+                    </Row>
+                    <Card.Text>
+                        <span className="text-muted">{note.content}</span>
+                    </Card.Text>
+                    <Col>
+                        <Card.Text>
+                            <span className="text-muted">{note.createdAt}</span>
+                        </Card.Text>
+                    </Col>
+                </Card.Body>
+            </Card.Text>
+        </>
     );
 };
 
