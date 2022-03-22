a0724af150229d9b94121767e7b5549a2f9f84c3
aeebbr <aeebbr@gmail.com>
2022-03-22 15:31:43 +0900

    [Modify] 전체 쪽지함 구성 변경

--- front/src/components/note/NoteListAll.js
+++ front/src/components/note/NoteListAll.js
@@ -1,13 +1,23 @@
-import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { Container, Form, Row, Col, Button, Card } from 'react-bootstrap';
 
-const NoteListAll = ({ key, note, setNotes }) => {
+const NoteListAll = ({ key, allNote, setAllNotes }) => {
     return (
-        <Row>
-            {note.title}
-            {note.content}
-        </Row>
-        
-    )
-}
+        <Card.Text as={Col}>
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <Card.Title>
+                            {allNote.toUser.name}에게 보낸 쪽지
+                        </Card.Title>
+                    </Col>
+                </Row>
+                <Card.Text>{allNote.title}</Card.Text>
+                <Card.Text>
+                    <span className="text-muted">{allNote.content}</span>
+                </Card.Text>
+            </Card.Body>
+        </Card.Text>
+    );
+};
 
-export default NoteListAll
+export default NoteListAll;
