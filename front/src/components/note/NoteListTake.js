698d30f3cedf461f44f7d4b74f01b17661c711e4
aeebbr <aeebbr@gmail.com>
2022-03-22 15:33:11 +0900

    [Modify] 수신 쪽지함 구성 변경, 수신 쪽지 삭제 구현

--- front/src/components/note/NoteListTake.js
+++ front/src/components/note/NoteListTake.js
@@ -1,12 +1,44 @@
-import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { Container, Form, Row, Col, Button, Card } from 'react-bootstrap';
+import * as Api from '../../api';
 
-const NoteListTake = ({ key, note, setNotes }) => {
+const NoteListTake = ({ key, takeNote, setTakeNote }) => {
+    const handleDelete = async (e) => {
+        e.preventDefault();
+
+        await Api.delete(`takenNotes/${takeNote.id}`);
+
+        // await Api.get('sentNotelist', user_id).then((res) => setSendNote(res.data));
+        await Api.get(`takenNotelist`).then((res) => {
+            setTakeNote(res.data);
+        });
+    };
+
     return (
-        <Row>
-            수신함
-            {/* {note.title}
-            {note.content} */}
-        </Row>
+        <Card.Text as={Col}>
+            <Card.Body>
+                <Row>
+                    <Col>
+                        <Card.Title>
+                            {takeNote.toUser.name}에게 보낸 쪽지
+                        </Card.Title>
+                    </Col>
+                    <Col>
+                        <Button
+                            variant="primary"
+                            size="sm"
+                            className="mvpCardCancelButton"
+                            onClick={handleDelete}
+                        >
+                            삭제
+                        </Button>{' '}
+                    </Col>
+                </Row>
+                <Card.Text>{takeNote.title}</Card.Text>
+                <Card.Text>
+                    <span className="text-muted">{takeNote.content}</span>
+                </Card.Text>
+            </Card.Body>
+        </Card.Text>
     );
 };
 
