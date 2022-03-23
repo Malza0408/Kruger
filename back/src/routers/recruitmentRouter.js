18b6f1db4a57be2b43c1b79ca6d6a952034768d5
DOHEE <dohee212@naver.com>
2022-03-23 12:24:10 +0900

    [Modify] router 정리

--- back/src/routers/recruitmentRouter.js
+++ back/src/routers/recruitmentRouter.js
@@ -6,42 +6,6 @@ import { RecruitmentService } from '../services/RecruitmentService';
 
 const recruitmentRouter = Router();
 
-recruitmentRouter.put(
-    '/recruit/:id',
-    login_required,
-    updateMiddleware,
-    async function (req, res, next) {
-        try {
-            const recruitmentId = req.params.id;
-            const user_id = req.currentUserId;
-            const toUpdate = req.toUpdate;
-
-            const updatedRecruitment = await RecruitmentService.setRecruitment({
-                recruitmentId,
-                user_id,
-                toUpdate
-            });
-
-            res.status(200).json(updatedRecruitment);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-// 게시글 하나 보기, 로그인 안해도 볼수있게.
-recruitmentRouter.get('/recruit/:id', async (req, res, next) => {
-    try {
-        const recruitmentId = req.params.id;
-        const recruitment = await RecruitmentService.getRecruitment({
-            recruitmentId
-        });
-        res.status(200).json(recruitment);
-    } catch (error) {
-        next(error);
-    }
-});
-
 // 게시글 생성
 recruitmentRouter.post(
     '/recruit/create',
@@ -62,6 +26,43 @@ recruitmentRouter.post(
                 detail
             });
             res.status(201).json(newRecruitment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+// 게시글 하나 보기, 로그인 안해도 볼수있게.
+recruitmentRouter.get('/recruit/:id', async (req, res, next) => {
+    try {
+        const recruitmentId = req.params.id;
+        const recruitment = await RecruitmentService.getRecruitment({
+            recruitmentId
+        });
+        res.status(200).json(recruitment);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 게시글 수정하기
+recruitmentRouter.put(
+    '/recruit/:id',
+    login_required,
+    updateMiddleware,
+    async function (req, res, next) {
+        try {
+            const recruitmentId = req.params.id;
+            const user_id = req.currentUserId;
+            const toUpdate = req.toUpdate;
+
+            const updatedRecruitment = await RecruitmentService.setRecruitment({
+                recruitmentId,
+                user_id,
+                toUpdate
+            });
+
+            res.status(200).json(updatedRecruitment);
         } catch (error) {
             next(error);
         }
@@ -128,6 +129,93 @@ recruitmentRouter.patch(
     }
 );
 
+// 멤버 승인하기
+recruitmentRouter.patch(
+    '/recruit/approval/:id',
+    login_required,
+    async function (req, res, next) {
+        try {
+            const { applicantId } = req.body;
+            const recruitmentId = req.params.id;
+            const user_id = req.currentUserId;
+
+            const updatedRecruitment = await RecruitmentService.setMember({
+                recruitmentId,
+                applicantId,
+                user_id
+            });
+            res.status(200).json(updatedRecruitment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+// 게시물에 좋아요 누르기
+recruitmentRouter.patch(
+    '/likedRecruit/:id',
+    login_required,
+    async function (req, res, next) {
+        try {
+            const recruitmentId = req.params.id;
+            const user_id = req.currentUserId;
+
+            const updatedRecruitment = await RecruitmentService.likeRecruitment(
+                {
+                    recruitmentId,
+                    user_id
+                }
+            );
+            res.status(200).json(updatedRecruitment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+// 게시글에 좋아요 해제하기
+recruitmentRouter.patch(
+    '/unlikedRecruit/:id',
+    login_required,
+    async function (req, res, next) {
+        try {
+            const recruitmentId = req.params.id;
+            const user_id = req.currentUserId;
+
+            const updatedRecruitment =
+                await RecruitmentService.unlikeRecruitment({
+                    recruitmentId,
+                    user_id
+                });
+            res.status(200).json(updatedRecruitment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+// 댓글 달기
+recruitmentRouter.put(
+    '/recruit/comment/:id',
+    login_required,
+    async function (req, res, next) {
+        try {
+            const { content } = req.body;
+            const recruitmentId = req.params.id;
+            const user_id = req.currentUserId;
+
+            const newComment = await RecruitmentService.addComment({
+                content,
+                recruitmentId,
+                user_id
+            });
+            res.status(200).json(newComment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
 // 댓글 수정하기
 recruitmentRouter.patch(
     '/recruit/comment/:id',
@@ -144,27 +232,6 @@ recruitmentRouter.patch(
                 toUpdate
             });
             res.status(200).json(updatedRecruitment);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-recruitmentRouter.put(
-    '/recruit/comment/:id',
-    login_required,
-    async function (req, res, next) {
-        try {
-            const { content } = req.body;
-            const recruitmentId = req.params.id;
-            const user_id = req.currentUserId;
-
-            const newComment = await RecruitmentService.addComment({
-                content,
-                recruitmentId,
-                user_id
-            });
-            res.status(200).json(newComment);
         } catch (error) {
             next(error);
         }
@@ -190,68 +257,7 @@ recruitmentRouter.patch(
     }
 );
 
-recruitmentRouter.patch(
-    '/likedRecruit/:id',
-    login_required,
-    async function (req, res, next) {
-        try {
-            const recruitmentId = req.params.id;
-            const user_id = req.currentUserId;
-
-            const updatedRecruitment = await RecruitmentService.likeRecruitment(
-                {
-                    recruitmentId,
-                    user_id
-                }
-            );
-            res.status(200).json(updatedRecruitment);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-recruitmentRouter.patch(
-    '/unlikedRecruit/:id',
-    login_required,
-    async function (req, res, next) {
-        try {
-            const recruitmentId = req.params.id;
-            const user_id = req.currentUserId;
-
-            const updatedRecruitment =
-                await RecruitmentService.unlikeRecruitment({
-                    recruitmentId,
-                    user_id
-                });
-            res.status(200).json(updatedRecruitment);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-recruitmentRouter.patch(
-    '/recruit/approval/:id',
-    login_required,
-    async function (req, res, next) {
-        try {
-            const { applicantId } = req.body;
-            const recruitmentId = req.params.id;
-            const user_id = req.currentUserId;
-
-            const updatedRecruitment = await RecruitmentService.setMember({
-                recruitmentId,
-                applicantId,
-                user_id
-            });
-            res.status(200).json(updatedRecruitment);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
+// 게시글 삭제하기
 recruitmentRouter.delete(
     '/recruit/:id',
     login_required,
