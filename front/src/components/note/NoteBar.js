2b0bd0b554737aaf20e1a6a26473b319f6bab3ca
aeebbr <aeebbr@gmail.com>
2022-03-23 22:04:58 +0900

    [Modify] 작성폼 레이아웃 수정

--- front/src/components/note/NoteBar.js
+++ front/src/components/note/NoteBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Container, Accordion, Form, Row, Col, Button } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import NoteWriteForm from './NoteWriteForm'
+import NoteWriteForm from './NoteWriteForm';
 
 const NoteBar = ({
     isWriting,
@@ -14,54 +14,56 @@ const NoteBar = ({
 }) => {
     const navigate = useNavigate();
     return (
-        <Row>
-            <Col>
-                <Button
-                    variant="primary"
-                    value="전체"
-                    className="me-3"
-                    onClick={() => setIsNoteListAll(true)}
-                >
-                    전체
-                </Button>
-            </Col>
-            <Col>
-                <Button
-                    variant="primary"
-                    value="발신"
-                    className="me-3"
-                    onClick={() => {
-                        setIsNoteListSending(true);
-                        setIsNoteListAll(false);
-                    }}
-                >
-                    발신함
-                </Button>
-            </Col>
-            <Col>
-                <Button
-                    variant="primary"
-                    value="발신"
-                    className="me-3"
-                    onClick={() => {
-                        setIsNoteListSending(false);
-                        setIsNoteListAll(false);
-                    }}
-                >
-                    수신함
-                </Button>
-            </Col>
-            <Col>
-                <Button
-                    variant="primary"
-                    value="전송"
-                    className="me-3"
-                    onClick={() => navigate('/note/write')}
-                >
-                    쪽지 작성
-                </Button>
-            </Col>
-        </Row>
+        <Container>
+            <Row>
+                <Col>
+                    <Button
+                        variant="primary"
+                        value="전체"
+                        className="me-3"
+                        onClick={() => setIsNoteListAll(true)}
+                    >
+                        전체
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        variant="primary"
+                        value="발신"
+                        className="me-3"
+                        onClick={() => {
+                            setIsNoteListSending(true);
+                            setIsNoteListAll(false);
+                        }}
+                    >
+                        발신함
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        variant="primary"
+                        value="발신"
+                        className="me-3"
+                        onClick={() => {
+                            setIsNoteListSending(false);
+                            setIsNoteListAll(false);
+                        }}
+                    >
+                        수신함
+                    </Button>
+                </Col>
+                <Col>
+                    <Button
+                        variant="primary"
+                        value="전송"
+                        className="me-3"
+                        onClick={() => navigate('/note/write')}
+                    >
+                        쪽지 작성
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
