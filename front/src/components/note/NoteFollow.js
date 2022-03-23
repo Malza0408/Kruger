25b4533ba9b53a23bb0f0f94ef345ca7417b9acd
aeebbr <aeebbr@gmail.com>
2022-03-24 00:57:31 +0900

    [Modify] 쪽지 팔로우 목록 cursor: pointer로 수정

--- front/src/components/note/NoteFollow.js
+++ front/src/components/note/NoteFollow.js
@@ -1,17 +1,18 @@
-import { Container, Row, Card } from 'react-bootstrap';
+import { Container, Row, Card, Col } from 'react-bootstrap';
 
 const NoteFollow = ({ follow, setTo }) => {
     return (
-        <Container>
-            <Card>
-                <Card.Body onClick={() => setTo(follow.email)}>
-                    <Row>
-                        <Row>{follow.name}</Row>
-                        <Row>{follow.email}</Row>
-                    </Row>
-                </Card.Body>
-            </Card>
-        </Container>
+        // <Container>
+        <Card.Text as={Col}>
+            <Card.Body style={{cursor: 'pointer'}} onClick={() => setTo(follow.email)}>
+                <Row>
+                    <Row>{follow.name}</Row>
+                    <Row>{follow.email}</Row>
+                </Row>
+            </Card.Body>
+            {/* <hr /> */}
+        </Card.Text>
+        // </Container>
     );
 };
 
