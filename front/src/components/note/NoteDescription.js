c00c6dae29a483ac184c52bfdb32adaa836aef2b
aeebbr <aeebbr@gmail.com>
2022-03-23 18:16:02 +0900

    [Modify] 쪽지 상세보기 date format 변경, 레이아웃 수정

--- front/src/components/note/NoteDescription.js
+++ front/src/components/note/NoteDescription.js
@@ -7,6 +7,7 @@ const NoteDescription = () => {
     const params = useParams();
     const [user, setUser] = useState(null);
     const [note, setNote] = useState('');
+    const [newDateFormatted, setNewDateFormatted] = useState('');
 
     useEffect(() => {
         Api.get(`user/current`).then((res) => setUser(res.data));
@@ -21,45 +22,80 @@ const NoteDescription = () => {
             Api.get(`takenNotes/${params.noteId}`).then((res) => {
                 setNote(res.data);
             });
-    }, [params, user]);
+    }, [params]);
+
+    useEffect(() => {
+        const newDate = new Date(note?.createdAt);
+
+        // date format을 'yyyy년 MM월 dd일 h:m:s'로 변경
+        const year = newDate.getFullYear();
+        const month = (newDate.getMonth() + 1).toString().padStart(2, '0');
+        const day = newDate.getDate().toString().padStart(2, '0');
+        const time = newDate.toLocaleString().split('.')[3];
+
+        setNewDateFormatted(`${year}년 ${month}월 ${day}일 ${time}`);
+    }, [note]);
+
+    console.log(note);
 
     return (
-        <>
-            <Card.Text as={Col}>
+        <Container fluid>
+            <Card>
                 <Card.Body>
                     <Card.Title>
                         {user?.name === note.fromUser?.name ? (
                             // 발신
-                            <span>
-                                <strong>{note.toUser?.name}</strong>
-                                <span className="text-muted">
-                                    <small>에게 보낸 쪽지</small>
-                                </span>
-                            </span>
+                            <Col>
+                                <Row>
+                                    <span class="fs-2">
+                                        <strong>{note.toUser?.name}</strong>
+                                        <span className="text-muted">
+                                            <small>에게 보낸 쪽지</small>
+                                        </span>
+                                    </span>
+                                </Row>
+                                <Row>
+                                    <span className="text-muted">
+                                        <small>{note.toUser?.email}</small>
+                                    </span>
+                                </Row>
+                            </Col>
                         ) : (
                             // 수신
-                            <span>
-                                <strong>{note.fromUser?.name}</strong>
-                                <span className="text-muted">
-                                    <small>가 보낸 쪽지</small>
-                                </span>
-                            </span>
+                            <Col>
+                                <Row>
+                                    <span class="fs-2">
+                                        <strong>{note.fromUser?.name}</strong>
+                                        <span className="text-muted">
+                                            <small>가 보낸 쪽지</small>
+                                        </span>
+                                    </span>
+                                </Row>
+                                <Row>
+                                    <span className="text-muted">
+                                        <small>{note.fromUser?.email}</small>
+                                    </span>
+                                </Row>
+                            </Col>
                         )}
+                        <Card.Text>
+                            <span className="fs-6 text-muted" >
+                                {newDateFormatted}
+                            </span>
+                        </Card.Text>
                     </Card.Title>
+                    <br />
                     <Card.Title>
-                        <span>
+                        <span class="fs-4">
                             <strong>{note.title}</strong>
                         </span>
                     </Card.Title>
                     <Card.Text>
-                        <span className="text-muted">{note.content}</span>
-                    </Card.Text>
-                    <Card.Text>
-                        <span className="text-muted">{note.createdAt}</span>
+                        <span className="fs-5 text-muted">{note.content}</span>
                     </Card.Text>
                 </Card.Body>
-            </Card.Text>
-        </>
+            </Card>
+        </Container>
     );
 };
 
