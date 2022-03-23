a9f398a02b732e6836e0319a0f2d4daa39da8b70
aeebbr <aeebbr@gmail.com>
2022-03-23 13:23:14 +0900

    [Modify] 수신 쪽지 상세 보기 구현

--- front/src/components/note/NoteDescription.js
+++ front/src/components/note/NoteDescription.js
@@ -16,65 +16,47 @@ const NoteDescription = () => {
         Api.get(`sentNotes/${params.noteId}`).then((res) => {
             setNote(res.data);
         });
-    }, [params]);
+
+        !user &&
+            Api.get(`takenNotes/${params.noteId}`).then((res) => {
+                setNote(res.data);
+            });
+    }, [params, user]);
 
     return (
         <>
             <Card.Text as={Col}>
                 <Card.Body>
-                    <Row>
-                        <Col>
-                            <Card.Title>
-                                <span>
-                                    {user?.name === note.fromUser?.name ? ( // 발신
-                                        <span>
-                                            <strong>{note.toUser?.name}</strong>
-                                            <span className="text-muted">
-                                                <small>에게 보낸 쪽지</small>
-                                            </span>
-                                        </span>
-                                    ) : (
-                                        // 수신
-                                        <span>
-                                            <strong>
-                                                {note.fromUser?.name}
-                                            </strong>
-                                            <span className="text-muted">
-                                                <small>가 보낸 쪽지</small>
-                                            </span>
-                                        </span>
-                                    )}
-                                    {/* <strong>{user?.name}</strong> */}
-                                </span>
-
-                                {/* <span className="text-muted">
-                                    <small>가 </small>
-                                </span>
-                                <span>
-                                    <strong>{note.toUser?.name}</strong>
-                                </span>
-
+                    <Card.Title>
+                        {user?.name === note.fromUser?.name ? (
+                            // 발신
+                            <span>
+                                <strong>{note.toUser?.name}</strong>
                                 <span className="text-muted">
                                     <small>에게 보낸 쪽지</small>
-                                </span> */}
-                            </Card.Title>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <span>
-                                <strong>{note.title}</strong>
+                                </span>
                             </span>
-                        </Col>
-                    </Row>
+                        ) : (
+                            // 수신
+                            <span>
+                                <strong>{note.fromUser?.name}</strong>
+                                <span className="text-muted">
+                                    <small>가 보낸 쪽지</small>
+                                </span>
+                            </span>
+                        )}
+                    </Card.Title>
+                    <Card.Title>
+                        <span>
+                            <strong>{note.title}</strong>
+                        </span>
+                    </Card.Title>
                     <Card.Text>
                         <span className="text-muted">{note.content}</span>
                     </Card.Text>
-                    <Col>
-                        <Card.Text>
-                            <span className="text-muted">{note.createdAt}</span>
-                        </Card.Text>
-                    </Col>
+                    <Card.Text>
+                        <span className="text-muted">{note.createdAt}</span>
+                    </Card.Text>
                 </Card.Body>
             </Card.Text>
         </>
