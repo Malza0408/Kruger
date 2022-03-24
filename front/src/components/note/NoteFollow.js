53712441af6ba368f64a07b6799379fac7a3a029
aeebbr <aeebbr@gmail.com>
2022-03-25 03:37:06 +0900

    [Add] 쪽지 작성 시 수신자 email 입력하면 해당 사용자의 name 띄우는 기능 구현

--- front/src/components/note/NoteFollow.js
+++ front/src/components/note/NoteFollow.js
@@ -1,10 +1,16 @@
 import { Container, Row, Card, Col } from 'react-bootstrap';
 
-const NoteFollow = ({ follow, setTo }) => {
+const NoteFollow = ({ follow, setTo, setName }) => {
     return (
         // <Container>
         <Card.Text as={Col}>
-            <Card.Body style={{cursor: 'pointer'}} onClick={() => setTo(follow.email)}>
+            <Card.Body
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                    setTo(follow.email);
+                    setName(follow.name);
+                }}
+            >
                 <Row>
                     <Row>{follow.name}</Row>
                     <Row>{follow.email}</Row>
