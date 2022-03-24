d484ca20bdce948baf112f6324e0c4fb0a4b66b3
Malza0408 <parkjh04080@gmail.com>
2022-03-24 17:41:02 +0900

    [Add]: post 내용 수정 가능

--- front/src/components/gather/Post.js
+++ front/src/components/gather/Post.js
@@ -5,20 +5,34 @@ import React, {
     useRef,
     useState
 } from 'react';
-import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import {
+    Button,
+    Container,
+    Form,
+    Row,
+    Col,
+    InputGroup,
+    DropdownButton,
+    Dropdown
+} from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
 import { patch, put, get } from '../../api';
 import { UserStateContext } from '../../App';
+import useGetLangFromDropDown from '../../custom/useGetLangFromDropDown';
 
 const Post = () => {
     const navigate = useNavigate();
     const userState = useContext(UserStateContext);
     const post_id = useParams();
     const [post, setPost] = useState();
-    const commentRef = useRef();
-    const [isLike, setIsLike] = useState(false);
-    const [isEditComment, setIsEditComment] = useState(false);
     const [comments, setComments] = useState([]);
+    // const []
+    const [langInputValue, setLangInputValue] = useState([]);
+    const [isEditPost, setIsEditPost] = useState(false);
+
+    const commentRef = useRef();
+    const titleRef = useRef();
+    const detailRef = useRef();
 
     const handleOnClickLike = async () => {
         try {
@@ -26,23 +40,24 @@ const Post = () => {
                 post.like.find((id) => id === userState.user.id) === undefined
             ) {
                 await patch(`likedRecruit/${post.id}`);
-                getPostDataWithoutComment();
+                getPostData();
             } else {
                 await patch(`unlikedRecruit/${post.id}`);
-                getPostDataWithoutComment();
+                getPostData();
             }
         } catch (error) {
             throw new Error(error);
         }
     };
 
-    const getPostData = useCallback(async () => {
+    const getPostDataWithComment = useCallback(async () => {
         const result = await get('recruit', post_id.id);
         setPost(result.data);
         setComments(result.data.comment);
+        console.log(result.data);
     }, [post_id.id]);
 
-    const getPostDataWithoutComment = useCallback(async () => {
+    const getPostData = useCallback(async () => {
         const result = await get('recruit', post_id.id);
         setPost(result.data);
     }, [post_id.id]);
@@ -54,7 +69,7 @@ const Post = () => {
             await put(`recruit/comment/${post.id}`, {
                 content
             });
-            getPostData();
+            getPostDataWithComment();
             commentRef.current.value = '';
         } catch (error) {
             throw new Error(error);
@@ -64,86 +79,250 @@ const Post = () => {
         return async () => {
             try {
                 await patch(`recruit/delete/${post.id}/${commentId}`);
-                getPostData();
+                getPostDataWithComment();
             } catch (error) {
                 throw new Error(error);
             }
         };
     };
 
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await put(`recruit/${post.id}`, {
+                title: titleRef.current.value,
+                detail: detailRef.current.value,
+                language: langInputValue.split(' / ')
+            });
+            getPostData();
+            setIsEditPost(!isEditPost);
+        } catch (error) {}
+    };
+
+    const handleToggleEditDetail = () => {
+        setLangInputValue(post.language.join(' / '));
+        setIsEditPost(!isEditPost);
+    };
+    const getLangFromDropDown = useGetLangFromDropDown({
+        langInputValue,
+        setLangInputValue
+    });
+
     useEffect(() => {
         if (!userState.user) {
             navigate('/login');
         }
-        getPostData();
-    }, [getPostData, navigate, userState.user]);
+        getPostDataWithComment();
+    }, [getPostDataWithComment, navigate, userState.user]);
     return (
-        <Container fluid className="text-center">
-            <h1>{post?.title}</h1>
-            <h4>{post?.captain?.name}</h4>
-            <h2>{post?.createdAt.substr(0, 10)}</h2>
-            <h2>
-                사용언어 :{' '}
-                {post?.language?.reduce((prev, cur, index) => {
-                    return prev + cur + ' ';
-                }, '')}
-            </h2>
-            <hr />
-            <pre>{post?.detail}</pre>
-            <img
-                src={`${process.env.PUBLIC_URL}/gatherImg/heart.png`}
-                alt="like"
-                width="20px"
-                height="20px"
-                className="me-1"
-                onClick={handleOnClickLike}
-            />
-            <span className="like-count">{post?.like.length}</span>
-            <Form onSubmit={handleOnSubmitComment}>
-                <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                    style={{ maxWidth: '900px', margin: 'auto' }}
-                >
-                    <Form.Label>댓글</Form.Label>
-                    <Form.Control
-                        placeholder="댓글을 입력하세요."
-                        as="textarea"
-                        rows={4}
-                        ref={commentRef}
+        <Container fluid className="text-center" style={{ maxWidth: '1000px' }}>
+            {isEditPost === false ? (
+                <>
+                    <Row>
+                        <h1>{post?.title}</h1>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h4>{post?.captain?.name}</h4>
+                        </Col>
+                        <Col>
+                            <h2>{post?.createdAt.substr(0, 10)}</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <h2>
+                            사용언어 :{' '}
+                            {post?.language?.reduce((prev, cur, index) => {
+                                return prev + cur + ' ';
+                            }, '')}
+                        </h2>
+                        <hr style={{ maxWidth: '900px', margin: 'auto' }} />
+                    </Row>
+                    <Row>
+                        <Col>
+                            <pre>{post?.detail}</pre>
+                        </Col>
+                    </Row>
+                    <img
+                        src={`${process.env.PUBLIC_URL}/gatherImg/heart.png`}
+                        alt="like"
+                        width="20px"
+                        height="20px"
+                        className="me-1"
+                        onClick={handleOnClickLike}
                     />
-                </Form.Group>
-                <Button type="submit">댓글 등록</Button>
-            </Form>
-            <ul>
-                {comments?.map((comment, index) => {
-                    return (
-                        <Row>
-                            <Col>
-                                {isEditComment ?? (
-                                    <li key={index}>{comment.content}</li>
-                                )}
-                            </Col>
-                            <Col>
-                                <Button
-                                    onClick={() =>
-                                        setIsEditComment(!isEditComment)
-                                    }
-                                >
-                                    수정
-                                </Button>
-                            </Col>
-                            <Col>
-                                <Button
-                                    onClick={handleOnClickDelete(comment.id)}
-                                >
-                                    삭제
-                                </Button>
-                            </Col>
-                        </Row>
-                    );
-                })}
-            </ul>
+
+                    <span className="like-count">{post?.like.length}</span>
+                    <Button onClick={() => handleToggleEditDetail()}>
+                        수정하기
+                    </Button>
+                    <Form onSubmit={handleOnSubmitComment}>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                            style={{ maxWidth: '900px', margin: 'auto' }}
+                        >
+                            <Form.Control
+                                placeholder="댓글을 입력하세요."
+                                as="textarea"
+                                rows={4}
+                                ref={commentRef}
+                            />
+                        </Form.Group>
+                        <Button type="submit">댓글 등록</Button>
+                        <ul>
+                            {comments?.map((comment, index) => {
+                                return (
+                                    <Row>
+                                        <Col>
+                                            <li key={index}>
+                                                {comment.content}
+                                            </li>
+                                        </Col>
+                                        <Col>
+                                            <Button
+                                                onClick={handleOnClickDelete(
+                                                    comment.id
+                                                )}
+                                            >
+                                                삭제
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                );
+                            })}
+                        </ul>
+                    </Form>
+                </>
+            ) : (
+                <Form onSubmit={handleOnSubmit}>
+                    <Form.Group controlId="posting" className="mb-3">
+                        {/* <Form.Label>제목</Form.Label> */}
+                        <Form.Control
+                            placeholder="제목을 작성해주세요."
+                            type="text"
+                            size="lg"
+                            ref={titleRef}
+                            defaultValue={post.title}
+                        />
+                    </Form.Group>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            type="text"
+                            disabled
+                            // onClick={handleToggle}
+                            placeholder="사용할 언어를 선택해주세요."
+                            value={langInputValue && langInputValue}
+                        />
+                        <Button
+                            variant="outline-secondary"
+                            id="button-addon2"
+                            className="deleteBtn"
+                            onClick={() => setLangInputValue('')}
+                        >
+                            Delete
+                        </Button>
+                        <DropdownButton
+                            variant="outline-secondary"
+                            title="Language"
+                            id="input-group-dropdown"
+                            align="end"
+                        >
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('JavaScript')}
+                            >
+                                JavaScript
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('TypeScript')}
+                            >
+                                TypeScript
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('Node.js')}
+                            >
+                                Node.js
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('React')}
+                            >
+                                React
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('Vue')}
+                            >
+                                Vue
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('Python')}
+                            >
+                                Python
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                href="#"
+                                onClick={(e) => {
+                                    getLangFromDropDown(e.target.innerText);
+                                    e.target.hidden = true;
+                                }}
+                                hidden={langInputValue.includes('Django')}
+                            >
+                                Django
+                            </Dropdown.Item>
+                        </DropdownButton>
+                    </InputGroup>
+                    <Form.Group controlId="posting" className="mt-3">
+                        <Form.Control
+                            as="textarea"
+                            rows={15}
+                            placeholder="프로젝트 및 스터디원을 구해보세요!"
+                            size="lg"
+                            ref={detailRef}
+                            defaultValue={post.detail}
+                        />
+                    </Form.Group>
+                    <Col className="text-end mt-3">
+                        <button
+                            className="postingBtn"
+                            onClick={() => handleToggleEditDetail()}
+                        >
+                            취소
+                        </button>
+                        <button className="postingBtn ms-3" type="submit">
+                            등록하기
+                        </button>
+                    </Col>
+                </Form>
+            )}
         </Container>
     );
 };
