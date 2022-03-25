80e44de1fe8d969c14c16fcc43bc36a5503910d6
guswn2521@gmail.com <guswn2521@gmail.com>
2022-03-25 17:24:00 +0900

    수정

--- back/src/services/GithubService.js
+++ back/src/services/GithubService.js
@@ -11,18 +11,18 @@ class GithubService {
     static async checkUser(userInfo) {
         const email = userInfo.email;
         const id = userInfo.id;
-        let user = await User.findByEmail({ email });
+        let user = await User.findById({ id });
         const loginMethod = userInfo.loginMethod;
         // 깃허브 인증은 거쳤으니까 이미 이메일이 등록된 유저라면 깃허브로 회원가입한 유저.
         // 토큰발급해서 로그인 시킴.
-        if (user && user.loginMethod === loginMethod) {
+        if (user) {
             const secretKey = process.env.JWT_SECRET_KEY || 'jwt-secret-key';
             const token = jwt.sign({ user_id: id }, secretKey);
             const { password, ...refinedUser } = user._doc;
             user._doc = { ...refinedUser, token };
             console.log('github user log in.');
             return user;
-        } else if (user && user.loginMethod === 'email') {
+        } else if (user.email === email) {
             const errorMessage =
                 '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.';
             throw new Error(errorMessage);
