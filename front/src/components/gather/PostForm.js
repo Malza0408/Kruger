721dd42da0c6ead0c4c87700dbce1f0b3fa57bd7
Malza0408 <parkjh04080@gmail.com>
2022-03-26 18:42:30 +0900

    [Modify]: post form

---
+++ front/src/components/gather/PostForm.js
@@ -0,0 +1,155 @@
+import React, { forwardRef } from 'react';
+import {
+    Button,
+    Col,
+    Dropdown,
+    DropdownButton,
+    Form,
+    InputGroup
+} from 'react-bootstrap';
+
+const PostForm = forwardRef(
+    (
+        {
+            handleSubmit,
+            post,
+            langInputValue,
+            setLangInputValue,
+            getLangFromDropDown,
+            handleToggleEditDetail
+        },
+        ref
+    ) => {
+        return (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="posting" className="mb-3">
+                    <Form.Control
+                        placeholder="제목을 작성해주세요."
+                        type="text"
+                        size="lg"
+                        ref={ref.titleRef}
+                        defaultValue={post.title}
+                    />
+                </Form.Group>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        type="text"
+                        disabled
+                        placeholder="사용할 언어를 선택해주세요."
+                        value={langInputValue && langInputValue}
+                        className="lang-input"
+                    />
+                    <Button
+                        variant="outline-secondary"
+                        id="button-addon2"
+                        className="lang-delete"
+                        onClick={() => setLangInputValue([])}
+                    >
+                        Delete
+                    </Button>
+                    <DropdownButton
+                        variant="outline-secondary"
+                        title="Language"
+                        id="input-group-dropdown"
+                        align="end"
+                    >
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('JavaScript')}
+                        >
+                            JavaScript
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('TypeScript')}
+                        >
+                            TypeScript
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('Node.js')}
+                        >
+                            Node.js
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('React')}
+                        >
+                            React
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('Vue')}
+                        >
+                            Vue
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('Python')}
+                        >
+                            Python
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                            href="#"
+                            onClick={(e) => {
+                                getLangFromDropDown(e.target.innerText);
+                                e.target.hidden = true;
+                            }}
+                            hidden={langInputValue.includes('Django')}
+                        >
+                            Django
+                        </Dropdown.Item>
+                    </DropdownButton>
+                </InputGroup>
+                <Form.Group controlId="posting" className="mt-3">
+                    <Form.Control
+                        as="textarea"
+                        rows={15}
+                        placeholder="프로젝트 및 스터디원을 구해보세요!"
+                        size="lg"
+                        ref={ref.detailRef}
+                        defaultValue={post.detail}
+                    />
+                </Form.Group>
+                <Col className="text-end mt-3">
+                    <button
+                        className="postingBtn"
+                        onClick={handleToggleEditDetail}
+                        type="button"
+                    >
+                        취소
+                    </button>
+                    <button className="postingBtn ms-3" type="submit">
+                        등록하기
+                    </button>
+                </Col>
+            </Form>
+        );
+    }
+);
+
+export default PostForm;
