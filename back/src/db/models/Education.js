929de4c705a9849664e462def3877e1a27b68e95
DOHEE <dohee212@naver.com>
2022-03-26 15:34:52 +0900

    [Modify] model 파일 코드 개선

--- back/src/db/models/Education.js
+++ back/src/db/models/Education.js
@@ -1,20 +1,25 @@
 import { EducationModel } from '../schemas/education';
 
 class Education {
+    // 새로운 학력 요소 작성, 학력과 관련된 내용 필요
     static async create({ newEducation }) {
         const createdNewEducation = await EducationModel.create(newEducation);
         return createdNewEducation;
     }
 
+    // 같은 user_id를 가진 모든 학력 요소들을 불러옴
+    static async findAll({ user_id }) {
+        const educations = await EducationModel.find({ user_id });
+        return educations;
+    }
+
+    // id를 통해 해당 학력 요소를 가져옴
     static async findById({ education_id }) {
         const education = await EducationModel.findOne({ id: education_id });
         return education;
     }
 
-    static async findAll({ user_id }) {
-        const educations = await EducationModel.find({ user_id });
-        return educations;
-    }
+    // id를 통해 해당 학력 요소를 수정함
     static async update(education_id, key, value) {
         const filter = { id: education_id };
         const update = { [key]: value };
@@ -26,6 +31,8 @@ class Education {
         );
         return updatedEducation;
     }
+
+    // id를 통해 해당 학력 요소를 삭제함
     static async deleteById({ education_id }) {
         await EducationModel.deleteOne({ id: education_id });
         return;
--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -1,16 +1,19 @@
 import { UserModel } from '../schemas/user';
 
 class User {
+    // 회원가입
     static async create(newUser) {
         const createdNewUser = await UserModel.create(newUser);
         return createdNewUser;
     }
 
+    // 모든 사용자 리스트를 가져옴
     static async findAll() {
         const users = await UserModel.find({});
         return users;
     }
 
+    // id를 통해 해당 사용자 정보를 가져옴
     static async findById(id) {
         const user = await UserModel.findOne({ id })
             .populate('follow')
@@ -18,16 +21,19 @@ class User {
         return user;
     }
 
+    // 이메일을 통해 해당 사용자 정보를 가져옴
     static async findByEmail({ email }) {
         const user = await UserModel.findOne({ email });
         return user;
     }
 
+    // 로그인 방법(이메일, 소셜)을 통해 사용자 리스트를 가져옴
     static async findByLoginMethod({ loginMethod }) {
         const user = await UserModel.findOne({ loginMethod });
         return user;
     }
 
+    // filter를 통해 특정한 사용자의 정보를 수정함
     static async update(filter, key, value) {
         const update = { [key]: value };
         const option = { returnOriginal: false };
@@ -40,6 +46,7 @@ class User {
         return updatedUser;
     }
 
+    // follow, follower의 정보를 수정함
     static async updateFollow(filter, newValue) {
         const option = { returnOriginal: false };
         const updatedUser = await UserModel.findOneAndUpdate(
@@ -52,6 +59,7 @@ class User {
         return updatedUser;
     }
 
+    // id를 통해 해당 사용자를 탈퇴시킴
     static async deleteById({ user_id }) {
         await UserModel.deleteOne({ id: user_id });
         return;
